65be649b7e7573e845d64c88c28422dccf8b7c9e
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-09-13 12:21:23 -0300

    Initial Commit

---
+++ swiftUI-013-pokedex/swiftUI_013_pokedexApp.swift
@@ -0,0 +1,20 @@
+//
+//  swiftUI_013_pokedexApp.swift
+//  swiftUI-013-pokedex
+//
+//  Created by Luiz Carlos da Silva Araujo on 13/09/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_013_pokedexApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
